18afd9c1ce22851142b4bc630feaf494aa1b898b
Duy Nguyen <duy_nobita@icloud.com>
2019-08-14 13:52:53 -0400

    Commit16: completed urls.py and views.py

--- pos_app/urls.py
+++ pos_app/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path, include
+from rest_framework import routers
+
 from . import views
 
-urlpatterns = [
+router = routers.DefaultRouter()
+router.register('users', views.UserView)
+router.register('tickets', views.TicketView)
+router.register('menus', views.MenuView)
 
+urlpatterns = [
+    path('', include(router.urls))
 ]
--- pos_app/views.py
+++ pos_app/views.py
@@ -1,7 +1,18 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 from .serializers import UserSerializer, TicketSerializer, MenuSerializer
 from .models import User, Ticket, Menu
 
 # Create your views here.
 
+class UserView(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class TicketView(viewsets.ModelViewSet):
+    queryset = Ticket.objects.all()
+    serializer_class = TicketSerializer
+
+class MenuView(viewsets.ModelViewSet):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
 
