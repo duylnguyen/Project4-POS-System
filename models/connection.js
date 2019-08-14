85376ab9f45f6f5efd7ab8b4db9f5e09b7175e5d
Duy Nguyen <duy_nobita@icloud.com>
2019-08-14 10:31:44 -0400

    Commit14: set up heroku for django

--- models/connection.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const connectionString = process.env.MONGODB_URI || "mongodb://localhost/POS-System";
-
-mongoose.connect(connectionString, { useNewUrlParser: true})
-  .then(() => {
-    console.log("connected to mongo at: " + connectionString);
-  });
-
-module.exports = mongoose
